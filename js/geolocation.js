899f760c89ca8f35e142632cc7792e26a924a505
Jonathan Speek <jspeek@jspeek-mbp13r.local>
2016-02-26 18:46:39 -0700

    added styling

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,5 +1,4 @@
 $(document).ready(function($) {
-
     $.ajax({
         url:"http://ipinfo.io",
         dataType:'jsonp',
@@ -44,6 +43,17 @@ $(document).ready(function($) {
                     } else {
                         $('#app').addClass('mild');
                     }
+
+                    var myColors = ["#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c",
+                        "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#7f8c8d"];
+                    var num = Math.floor(Math.random() * myColors.length);
+                    var color = myColors[num];
+                    document.getElementById("color1").style.backgroundColor = color;
+                    document.getElementById("color2").style.color = color;
+                    document.getElementById("color3").style.color = color;
+                    document.getElementsByClassName("color4")[0].style.color = color;
+                    document.getElementsByClassName("color5")[0].style.color = color;
+                    document.getElementsByClassName("color6")[0].style.backgroundColor = color;
 
                     $('#temp-btn').click(function() {
 
